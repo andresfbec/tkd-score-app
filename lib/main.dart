a763bdef8168832f032909e4b590f71aced72711
CodeAndrew17 <becerrafelipe8b@gmail.com>
2025-11-23 19:13:31 -0500

    Add initial folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,50 @@
-import 'package:flutter/material.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:path/path.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Inicializar FFI para escritorio
-  sqfliteFfiInit();
-  databaseFactory = databaseFactoryFfi;
-
-  final db = await openDatabase(
-    join(await databaseFactory.getDatabasesPath(), 'test.db'),
-    onCreate: (db, version) async {
-      await db.execute('CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT)');
-    },
-    version: 1,
-  );
-
-  await db.insert('dogs', {'id': 1, 'name': 'Firulais'});
-  final result = await db.query('dogs');
-  print(result); // debería mostrar [{id: 1, name: Firulais}]
-
-  runApp(const MyApp());
+void main() {
+  runApp(const MyApp()); //punto de entrada a la aplicación Flutter
 }
 
+//esta es una pantalla y aca definimos las cosas globales de la aplicacion como titulo y esas cosas
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); //constructor con super.key
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('SQFLite FFI funcionando ✅')),
+    return FluentApp(
+      title: 'Mi primera app',
+      home: const HomePage(), //pantalla principal de la aplicación
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+//vista pantalla principal del aplicativo (lo primero q ve el usuario)
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldPage(
+      //esto de scaffolpage es el 'andamio' o estructura de la pantalla
+      header: const PageHeader(title: Text('🥋 TKD Nexus')),
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Hola, bienvenido esta es primera pagina web construida en flutter',
+          ),
+          const Text('Este es un segundo texto por practicar'),
+          const SizedBox(height: 20, width: 10,),
+          TextBox(
+            placeholder: 'Tu nombre aqui',
+            onChanged: (valor) =>  print('el valor fue $valor')
+          ),
+          SizedBox(height: 20, width: 1,),
+          Button(
+            child: const Text('Imprime lo que escribiste'),
+            onPressed: () => print('hola mundo')
+          ),
+        ],
       ),
     );
   }
